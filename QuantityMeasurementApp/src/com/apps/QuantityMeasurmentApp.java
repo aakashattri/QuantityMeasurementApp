c0ae39f14bfbcd721fd99d94983c87c22d78819a
Aakash Attri <aakashattri516@gmail.com>
2026-02-18 18:07:42 +0530

    [Aakash] Added : feature 2 in (feature/UC2-FeetAndInchesMeasurementEquality

---
+++ QuantityMeasurementApp/src/com/apps/QuantityMeasurmentApp.java
@@ -0,0 +1,66 @@
+
+
+public class QuantityMeasurmentApp {
+	public static class Feet{
+		private final double value;
+		
+		Feet(double value){
+			this.value=value;
+		}
+		public double getValue() {
+			return value;
+		}
+		
+		
+		@Override
+		public boolean equals(Object obj) {
+			if(this==obj) {
+				return true;
+			}
+			if(obj==null || getClass()!=obj.getClass()) {
+				return false;
+			}
+			Feet other=(Feet)obj;
+			
+			return Double.compare(this.value, other.value)==0;
+		}
+		
+	}
+	public static class Inches{
+		private final double value;
+		
+		Inches(double value){
+			this.value=value;
+		}
+		public double getValue() {
+			return this.value;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if(this==obj) {
+				return true;
+			}
+			if(obj==null||getClass()!=obj.getClass()) {
+				return false;
+			}
+			Inches other=(Inches)obj;
+			return Double.compare(this.value, other.value)==0;
+		}
+	}
+	public static void demonstrateFeetEquality() {
+		Feet f1=new Feet(1.2);
+		Feet f2=new Feet(1.2);
+		System.out.println("Equals("+f1.equals(f2)+")");
+	}
+	public static void demonstrateInchesEquality() {
+		Inches f1=new Inches(1.2);
+		Inches f2=new Inches(4.5);
+		System.out.println("Equals("+f1.equals(f2)+")");
+	}
+	public static void main(String[] args) {
+		demonstrateFeetEquality();
+		demonstrateInchesEquality();
+		
+	}
+}
+
---
+++ QuantityMeasurementApp/src/com/apps/QuantityMeasurmentAppTest.java
@@ -0,0 +1,64 @@
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class QuantityMeasurmentAppTest {
+
+
+    @Test
+    void testInchesEquality_SameValue() {
+        assertEquals(new QuantityMeasurmentApp.Inches(2.5), new QuantityMeasurmentApp.Inches(2.5));
+    }
+
+    @Test
+    void testInchesEquality_DifferentValue() {
+        assertNotEquals(new QuantityMeasurmentApp.Inches(5.0), new QuantityMeasurmentApp.Inches(7.0));
+    }
+
+    @Test
+    void testInchesEquality_NullComparison() {
+        assertNotEquals(new QuantityMeasurmentApp.Inches(5.2), null);
+    }
+
+    @Test
+    void testInchesEquality_NonNumericInput() {
+        assertNotEquals(new QuantityMeasurmentApp.Inches(4.1), "Other String");
+    }
+
+    @Test
+    void testInchesEquality_SameReference() {
+        QuantityMeasurmentApp.Inches f1=new QuantityMeasurmentApp.Inches(8.3);
+        assertEquals(f1,f1);
+    }
+
+    // FEET TESTS
+
+    @Test
+    void testFeetEquality_SameValue() {
+        assertEquals(new QuantityMeasurmentApp.Feet(2.5), new QuantityMeasurmentApp.Feet(2.5));
+    }
+
+    @Test
+    void testFeetEquality_DifferentValue() {
+        assertNotEquals(new QuantityMeasurmentApp.Feet(5.0), new QuantityMeasurmentApp.Feet(7.0));
+    }
+
+    @Test
+    void testFeetEquality_NullComparison() {
+        assertNotEquals(new QuantityMeasurmentApp.Feet(5.2), null);
+    }
+
+    @Test
+    void testFeetEquality_NonNumericInput() {
+        assertNotEquals(new QuantityMeasurmentApp.Feet(4.1), "Other String");
+    }
+
+    @Test
+    void testFeetEquality_SameReference() {
+        QuantityMeasurmentApp.Feet f1=new QuantityMeasurmentApp.Feet(8.3);
+        assertEquals(f1,f1);
+    }
+}
